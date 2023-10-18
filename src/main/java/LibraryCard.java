32e7060ea300b28d7017cea75dbc708aca811f84
Vidhathra <s3944867@student.rmit.edu.au>
2023-10-19 05:28:11 +1100

    setting method written for test prep

--- src/main/java/LibraryCard.java
+++ src/main/java/LibraryCard.java
@@ -4,11 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Library Card associated with the Student 
+ * Library Card associated with the Student
  */
 public class LibraryCard {
     /**
-     * Card id 
+     * Card id
      */
     private int ID;
 
@@ -33,20 +33,16 @@ public class LibraryCard {
     private double fine;
 
     /**
-     *  Details about the cardholder
+     * Details about the cardholder
      */
     private Student student;
-
-
-
 
     public LibraryCard(Student student, Date IssueDate, Date ExpiryDate, int ID) {
         this.student = student;
         this.IssueDate = IssueDate;
-	   this.ExpiryDate = ExpiryDate;
-	   this.ID = ID;
+        this.ExpiryDate = ExpiryDate;
+        this.ID = ID;
     }
-
 
     public int getID() {
         return this.ID;
@@ -64,7 +60,6 @@ public class LibraryCard {
         this.fine = fine;
     }
 
-
     public Date getIssueDate() {
         return IssueDate;
     }
@@ -81,25 +76,47 @@ public class LibraryCard {
         this.ExpiryDate = ExpiryDate;
     }
 
-    
     public List<Book> getBooks() {
         return borrowed;
     }
 
-    
-
     /**
      * Issue a new book
-     * @param Book: book to borrow 
+     * 
+     * @param Book: book to borrow
      * @return true if the book is successfully borrowed, false otherwise
      */
 
-    public boolean issueBook(Book book){
-    	return false;
-   
+    public boolean issueBook(Book book) {
+        int numbooks = getBooks().size();
+        boolean book_borrowed = borrowed.contains(book);
+
+        if (numbooks <= 4) {
+            if (!book_borrowed) {
+                return true;
+            }
+
+        }
+        return false;
+
     }
 
+    private boolean isBookAlreadyBorrowed(Book book) {
+        return borrowed.contains(book);
+    }
 
+    private boolean isCardStillValid() {
+        Date currentDate = new Date();
+        return ExpiryDate.after(currentDate);
+    }
 
+    private boolean isBookAvailableForBorrowing(Book book) {
+        // Implement logic to check book availability
+        return true; // Placeholder, update as needed
+    }
+
+    private boolean noPendingFine() {
+        return fine == 0.0;
+    }
 
 }
--- src/test/java/IssueBook.java
+++ src/test/java/IssueBook.java
@@ -8,16 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Date;
 
 /**
- *  Implement and test {Programme.addStudent } that respects the considtion given the assignment specification
- * NOTE: You are expected to verify that the constraints to borrow a new book from a library
+ * Implement and test {Programme.addStudent } that respects the considtion given
+ * the assignment specification
+ * NOTE: You are expected to verify that the constraints to borrow a new book
+ * from a library
  *
  * Each test criteria must be in an independent test method .
  *
  * Initialize the test object with "setting" method.
  */
 public class IssueBook {
-	
-	
 
+    private LibraryCard libraryCard;
+
+    // setting method
+    public void initializeLibraryCard() {
+        // create new LibraryCard with a student, valid dates, and no books or fines
+        Student student = new Student("Vanessa Smith", 1234567);
+        Date issueDate = new Date();
+        Date expiryDate = new Date(System.currentTimeMillis() + 10000000); // Set an expiry date in the future
+        libraryCard = new LibraryCard(student, issueDate, expiryDate, 1);
+    }
 
 }
